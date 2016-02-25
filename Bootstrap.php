a02d77a9d1a5c0bb55c219c11a120245faf48ca5
Stefan Matheis <stefan@kellerkinder.de>
2016-02-25 17:05:36 +0100

    initial commit

---
+++ Bootstrap.php
@@ -0,0 +1,236 @@
+<?php
+
+class Shopware_Plugins_Core_VersionCentralTracker_Bootstrap extends Shopware_Components_Plugin_Bootstrap
+{
+    /**
+     * @var array
+     */
+    protected $pluginInfo = [];
+
+    /**
+     * @return array
+     */
+    public function getCapabilities()
+    {
+        return [
+            'install' => true,
+            'enable'  => true,
+            'update'  => true,
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getInfo()
+    {
+        return [
+            'version'     => $this->getVersion(),
+            'author'      => $this->getPluginInfo()['author'],
+            'label'       => $this->getLabel(),
+            'description' => str_replace('%label%', $this->getLabel(), file_get_contents(sprintf('%s/plugin.txt', __DIR__))),
+            'copyright'   => $this->getPluginInfo()['copyright'],
+            'support'     => $this->getPluginInfo()['support'],
+            'link'        => $this->getPluginInfo()['link'],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    protected function getPluginInfo()
+    {
+        if ($this->pluginInfo === []) {
+            $file = sprintf('%s/plugin.json', __DIR__);
+
+            if (!file_exists($file) || !is_file($file)) {
+                throw new \RuntimeException('The plugin has an invalid version file.');
+            }
+
+            $this->pluginInfo = json_decode(file_get_contents($file), true);
+        }
+
+        return $this->pluginInfo;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel()
+    {
+        return (string) $this->getPluginInfo()['label']['de'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        return $this->getPluginInfo()['currentVersion'];
+    }
+
+    /**
+     * @return bool
+     */
+    public function install()
+    {
+        return $this->createEvents();
+    }
+
+    /**
+     * @param string $oldVersion
+     *
+     * @return bool
+     */
+    public function update($oldVersion)
+    {
+        return $this->createEvents($oldVersion);
+    }
+
+    /**
+     * @return bool
+     */
+    public function uninstall()
+    {
+        return true;
+    }
+
+    /**
+     * @throws Exception
+     * @return bool
+     */
+    public function enable()
+    {
+        $httpClient = new Zend_Http_Client('http://data.version-central.vm');
+        $httpClient->setHeaders('Accept', 'application/vnd.version-central-v1+json');
+        $httpClient->setAuth(
+            $this->Config()->get('versionCentralApiIdentifier'),
+            $this->Config()->get('versionCentralApiToken')
+        );
+        $response = $httpClient->request($httpClient::HEAD);
+
+        if (intval($response->getStatus()/100) !== 2) {
+            throw new Exception(__METHOD__);
+        }
+
+        return true;
+    }
+
+    public function afterInit()
+    {
+        $this->get('Loader')->registerNamespace(
+            'Shopware\Plugins\VersionCentralTracker',
+            $this->Path()
+        );
+    }
+
+    /**
+     * @param null|string $oldVersion
+     *
+     * @return bool
+     */
+    private function createEvents($oldVersion = null)
+    {
+        $versionClosures = [
+
+            '0.0.1' => function (Shopware_Plugins_Core_VersionCentralTracker_Bootstrap $bootstrap) {
+                $bootstrap->addConfigurationForm();
+
+                $bootstrap->subscribeEvent(
+                    'Shopware_Console_Add_Command',
+                    'onConsoleAddCommand'
+                );
+
+                $bootstrap->subscribeEvent(
+                    'Shopware_Controllers_Backend_Config::saveFormAction::after',
+                    'afterConfigSave'
+                );
+
+                return true;
+            },
+        ];
+
+        foreach ($versionClosures as $version => $versionClosure) {
+            if (version_compare($oldVersion, $this->getVersion(), '<')) {
+                if (!$versionClosure($this)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public function addConfigurationForm()
+    {
+        $form = $this->Form();
+
+        $form->setParent(
+            $this->Forms()->findOneBy(['name' => 'Interface'])
+        );
+
+        $form->setElement(
+            'text',
+            'versionCentralApiIdentifier',
+            [
+                'label' => 'API Identifier',
+                'value' => null,
+                'required' => true,
+                'scope' => Shopware\Models\Config\Element::SCOPE_SHOP
+            ]
+        );
+
+        $form->setElement(
+            'text',
+            'versionCentralApiToken',
+            [
+                'label' => 'API Token',
+                'value' => null,
+                'required' => true,
+                'scope' => Shopware\Models\Config\Element::SCOPE_SHOP
+            ]
+        );
+    }
+
+    /**
+     * @param Enlight_Event_EventArgs $args
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function onConsoleAddCommand(Enlight_Event_EventArgs $args)
+    {
+        return new \Doctrine\Common\Collections\ArrayCollection([
+            new \Shopware\Plugins\VersionCentralTracker\Commands\UpdateCommand(),
+        ]);
+    }
+
+    public function afterConfigSave(Enlight_Event_EventArgs $args)
+    {
+        $request = $args->getSubject()->Request();
+        $values = [];
+        foreach ($request->getParam('elements') as $element) {
+            $values[$element['name']] = $element['values'][0]['value'];
+        }
+
+        $httpClient = new Zend_Http_Client('http://data.version-central.vm');
+        $httpClient->setHeaders('Accept', 'application/vnd.version-central-v1+json');
+        $httpClient->setAuth(
+            $values['versionCentralApiIdentifier'],
+            $values['versionCentralApiToken']
+        );
+        $response = $httpClient->request($httpClient::HEAD);
+
+        if (intval($response->getStatus()/100) === 2) {
+            $result = [
+                'success' => true
+            ];
+        } else {
+            $result = [
+                'success' => false,
+                'message' => 'AUTHORIZATION_INVALID'
+            ];
+        }
+
+        $args->getSubject()->View()->assign($result);
+        return;
+    }
+}
---
+++ Commands/UpdateCommand.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Shopware\Plugins\VersionCentralTracker\Commands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Shopware\Commands\ShopwareCommand;
+use Shopware\Models\Plugin\Plugin;
+use Shopware;
+
+use Zend_Http_Client;
+
+use DomainException;
+
+class UpdateCommand extends ShopwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('VersionCentral:Update')
+            ->setDescription('Update version information for plugins and application.')
+            ->setHelp('The <info>%command.name%</info> updates version information of plugins and the application.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $builder = $this->container->get('models')
+            ->getRepository('Shopware\Models\Plugin\Plugin')
+            ->createQueryBuilder('plugin')
+            ->andWhere('plugin.capabilityEnable = true');
+
+        $plugins = array_map(
+            function(Plugin $plugin) {
+                return [
+                    'identifier' => $plugin->getName(),
+                    'version' => $plugin->getVersion(),
+                    'active' => $plugin->getActive()
+                ];
+            },
+            $builder->getQuery()->execute()
+        );
+
+        $data = [
+            'application' => [
+                'identifier' => 'shopware',
+                'version' => Shopware::VERSION
+            ],
+            'packages' => $plugins
+        ];
+
+        $config = $this->getContainer()->get('config');
+
+        $httpClient = new Zend_Http_Client('http://data.version-central.vm');
+        $httpClient->setHeaders('Accept', 'application/vnd.version-central-v1+json');
+        $httpClient->setAuth(
+            $config->getByNamespace('VersionCentralTracker', 'versionCentralApiIdentifier'),
+            $config->getByNamespace('VersionCentralTracker', 'versionCentralApiToken')
+        );
+        $httpClient->setRawData(json_encode($data), 'application/json');
+        $response = $httpClient->request($httpClient::PUT);
+        
+        if (intval($response->getStatus()/100) !== 2) {
+            $errors = array_map(
+                function(array $error) {
+                    unset($error['schema']);
+                    return $error;
+                },
+                json_decode($response->getBody(), true)
+            );
+            throw new DomainException(print_r($errors, true));
+        }
+
+        $output->writeln(sprintf('<info>Versions successfully updated.</info>'));
+    }
+}
